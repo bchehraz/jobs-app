6ee6852aa82d11b2003be1b78233791551eb40d4
Babak Chehraz <bchehraz@csumb.edu>
2017-10-06 16:50:38 -0700

    Initial project commit

---
+++ App.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { StyleSheet, View, Platform } from 'react-native';
+import { TabNavigator, StackNavigator } from 'react-navigation';
+
+import AuthScreen from './screens/AuthScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
+import MapScreen from './screens/MapScreen';
+import DeckScreen from './screens/DeckScreen';
+import SettingsScreen from './screens/SettingsScreen';
+import ReviewScreen from './screens/ReviewScreen';
+
+export default class App extends React.Component {
+  render() {
+    const MainNavigator = TabNavigator({
+      welcome: { screen: WelcomeScreen },
+      auth: { screen: AuthScreen },
+      main: {
+        screen: TabNavigator({
+          map: { screen: MapScreen },
+          deck: { screen: DeckScreen },
+          review: {
+            screen: StackNavigator({
+              review: { screen: ReviewScreen },
+              settings: { screen: SettingsScreen }
+            })
+          }
+        }, {
+          lazyLoad: true,
+          swipeEnabled: false,
+          animationEnabled: false,
+          tabBarPosition: 'bottom'
+        })
+      }
+    }, {
+      lazyLoad: true,
+      swipeEnabled: false,
+      animationEnabled: false,
+      tabBarPosition: 'bottom'
+    });
+
+    return (
+      <View style={styles.container}>
+        <MainNavigator />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+		backgroundColor: '#fff',
+    justifyContent: 'center',
+    marginTop: Platform.OS === 'android' ? 24 : 0
+  },
+});
